07ed8271d44b0364802f49476098dc0a67b592f2
Maria Vodolazkaya <vodolazkayamv@gmail.com>
2021-12-05 14:27:19 +0300

    загрузка фоток

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -22,22 +22,27 @@ const Post = ( post ) => {
 
     const [barrier, setBarrier] = useState(null); 
     const [barrierInfo, setBarrierInfo] = useState(null); 
+    const [observations, setObservations] = useState(null); 
+
+    const [image, setImage] = useState(null);
+    const [createObjectURL, setCreateObjectURL] = useState(null);
 
     useEffect(() => {
         getBarriers()
         getBarrierInfo()
+        getObservations()
     }, []);
     const getBarriers = async (event) => {
                
         const response = await axios.get('https://lapki.appmat.org/api/barriers/'+slug);
-        console.log('response', response.data);
+        // console.log('response', response.data);
         setBarrier(response.data);
       };
 
       const getBarrierInfo = async (event) => {
                
         const response = await axios.get('https://lapki.appmat.org/api/barriers/'+slug+'/observations');
-        console.log('response', response.data);
+        // console.log('response', response.data);
         setBarrierInfo(response.data);
       };
 
@@ -45,7 +50,7 @@ const Post = ( post ) => {
       var yData = []
       var sum = 0
       post.days.forEach((day) => {
-        console.log('day', day)
+        // console.log('day', day)
         var date = new Date(day.date)
         xData.push(date)
         yData.push(day.fishes)
@@ -81,6 +86,82 @@ const Post = ( post ) => {
         }
       };
 
+
+      const uploadToClient = (event) => {
+        if (event.target.files && event.target.files[0]) {
+          const i = event.target.files[0];
+    
+          setImage(i);
+          setCreateObjectURL(URL.createObjectURL(i));
+        }
+      };
+
+      const uploadToServer = async (event) => {
+        const config = {
+            headers: { 'content-type': 'multipart/form-data' },
+            onUploadProgress: (event) => {
+              console.log(`Current progress:`, Math.round((event.loaded * 100) / event.total));
+            },
+          };
+
+          Date.prototype.yyyymmdd = function() {
+            var mm = this.getMonth() + 1; // getMonth() is zero-based
+            var dd = this.getDate();
+            var hours = this.getHours();
+            var minutes = this.getMinutes();
+          
+            return [this.getFullYear(),
+                    (mm>9 ? '' : '0') + mm,
+                    (dd>9 ? '' : '0') + dd,
+                   ].join('-');
+          };
+          
+          var date = new Date();
+          date.yyyymmdd();
+
+        const body = new FormData();
+        body.append("file", image);
+        body.append("date",  date.yyyymmdd());
+        
+        const response = await axios.post('https://lapki.appmat.org/api/barriers/'+slug+'/observations', body, config);
+        console.log('response', response.data);
+
+        getObservations()
+      };
+
+      const getObservations = async (event) => {
+
+        const response = await axios.get('https://lapki.appmat.org/api/barriers/2/observations');
+        console.log(response)
+        function filterByID(item) {
+            if (item.barrierId == slug) {
+              return true
+            }
+            return false;
+          }
+
+          let observationsByID = response.data.filter(filterByID)
+        //   console.log(observationsByID)
+          setObservations(observationsByID)
+
+          
+      }
+
+      var observationsTrace = {
+        x: [],
+        y: [],
+        type: 'bar'
+      }
+      if (observations) {
+      observations.forEach((observation) => {
+          var date = new Date(observation.date)
+          var fishes = observation.fish
+          observationsTrace.x.push(date)
+          observationsTrace.y.push(fishes)
+      })
+    }
+      var observationsData = [observationsTrace]
+
     return (
 
         <Article>
@@ -88,6 +169,22 @@ const Post = ( post ) => {
             
             <PlotComponent data={data}  layout={layout} />
             <PlotComponent data={dataIndicator} />
+
+            <h1>Загружено:</h1>
+            <input type="file" name="image" onChange={uploadToClient} multiple="multiple" />
+
+
+            <PlotComponent data={observationsData}  layout={layout} />
+
+        <button
+          className="btn btn-primary"
+          type="submit"
+          onClick={uploadToServer}
+        >
+          Send to server
+        </button>
+
+
         </Article>
 
     )
